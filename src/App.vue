f72a01aaff005fb522f44a8925c284e52d68e998
k-kudo-hub <tongariisaibou@gmail.com>
2020-11-10 08:14:42 +0900

    ライフサイクルフックのactivatedとdeactivatedを使用する

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,9 @@
     <LikeNumber :total-number="number" @my-click="incrementNumber"></LikeNumber>
     <button @click="currentCompornent = 'Home'">Home</button>
     <button @click="currentCompornent = 'About'">About</button>
-    <component :is="currentCompornent"></component>
+    <keep-alive>
+      <component :is="currentCompornent"></component>
+    </keep-alive>
   </div>
 </template>
 
