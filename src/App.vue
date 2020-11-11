6f68b4b7a127b3e455e1cf03e854a96604306513
k-kudo-hub <tongariisaibou@gmail.com>
2020-11-11 10:51:23 +0900

    単体チェックボックスの双方向バインディング

--- src/App.vue
+++ src/App.vue
@@ -14,6 +14,19 @@
       <label for="maxNumber">最大人数</label>
       <input id="maxNumber" type="number" v-model.number="eventData.maxNumber">
       <p>{{ eventData.maxNumber }}</p>
+      <label for="host">主催者</label>
+      <input 
+        id="host"
+        type="text" 
+        v-model.trim="eventData.host"
+      >
+      <p>{{ eventData.host }}</p>
+      <label for="detail">イベントの内容</label>
+      <textarea id="" cols="30" rows="10" v-model="eventData.detail"></textarea>
+      <pre>{{ eventData.detail }}</pre>
+      <input type="checkbox" id="isPrivate" v-model="eventData.isPrivate">
+      <label for="isPrivate">非公開</label>
+      <p>{{ eventData.isPrivate }}</p>
     </div>
   </div>
 </template>
@@ -29,7 +42,11 @@ export default {
       number: 14,
       currentCompornent: "Home",
       eventData: {
-        title: "タイトル",
+        title: "",
+        maxNumber: 0,
+        host: "",
+        detail: "",
+        isPrivate: false
       }
     };
   },
