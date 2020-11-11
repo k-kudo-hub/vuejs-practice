d9f7b638c82e27f0453bb6d3048be70ca1e8fd66
k-kudo-hub <tongariisaibou@gmail.com>
2020-11-11 10:30:07 +0900

    .number修飾子で入力を数値として型変換する

--- src/App.vue
+++ src/App.vue
@@ -11,9 +11,9 @@
     </keep-alive>
     <div>
       <h2>イベントのフォーム</h2>
-      <label for="title">タイトル</label>
-      <input id="title" type="text" v-model.lazy="eventData.title">
-      <p>{{eventData.title}}</p>
+      <label for="maxNumber">最大人数</label>
+      <input id="maxNumber" type="number" v-model.number="eventData.maxNumber">
+      <p>{{ eventData.maxNumber }}</p>
     </div>
   </div>
 </template>
