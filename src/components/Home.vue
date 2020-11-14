3269b6b62b1b855542a418d0e22daaabfc1ef456
k-kudo-hub <tongariisaibou@gmail.com>
2020-11-14 12:55:38 +0900

    2つ以上の修飾子をカスタムディレクティブに付与する

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,4 +1,4 @@
 <template>
-  <p v-border:dotted.round="{width: '5px', color: 'red'}">Home</p>
+  <p v-border:dotted.round.shadow="{width: '5px', color: 'red'}">Home</p>
   <!-- （注）引数は一つしか取れない -->
 </template>
