56eee782ff7dabf3b38dd94f00ca3bf569fc1be5
k-kudo-hub <tongariisaibou@gmail.com>
2020-11-14 11:06:19 +0900

    カスタムディレクティブの作成

--- src/main.js
+++ src/main.js
@@ -4,6 +4,23 @@ import LikeNumber from "./components/LikeNumber.vue";
 
 Vue.config.productionTip = false
 Vue.component('LikeNumber',LikeNumber);
+Vue.directive("border",{
+  bind(el, binding, vnode) {
+    // ディレクティブが初めて対象の要素に紐づいたとき
+  },
+  inserted(el, binding, vnode) {
+    // 親Nodeに挿入されたとき
+  },
+  update(el, binding, vnode, oldvnode) {
+    // コンポーネントが更新される度。子コンポーネントが更新される前
+  },
+  componentUpdated(el, binding, vnode, oldvnode) {
+    // コンポーネントが更新される度。子コンポーネントが更新された後
+  },
+  unbind(el, binding, vnode) {
+    // ディレクティブが紐づいている要素から取り除かれたとき
+  },
+});
 
 new Vue({
   render: h => h(App),
