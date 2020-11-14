184adce63430d89550c1774492dd866417d7d175
k-kudo-hub <tongariisaibou@gmail.com>
2020-11-14 11:09:26 +0900

    カスタムディレクティブの省略記法

--- src/main.js
+++ src/main.js
@@ -4,22 +4,8 @@ import LikeNumber from "./components/LikeNumber.vue";
 
 Vue.config.productionTip = false
 Vue.component('LikeNumber',LikeNumber);
-Vue.directive("border",{
-  bind(el, binding, vnode) {
-    // ディレクティブが初めて対象の要素に紐づいたとき
-  },
-  inserted(el, binding, vnode) {
-    // 親Nodeに挿入されたとき
-  },
-  update(el, binding, vnode, oldvnode) {
-    // コンポーネントが更新される度。子コンポーネントが更新される前
-  },
-  componentUpdated(el, binding, vnode, oldvnode) {
-    // コンポーネントが更新される度。子コンポーネントが更新された後
-  },
-  unbind(el, binding, vnode) {
-    // ディレクティブが紐づいている要素から取り除かれたとき
-  },
+Vue.directive("border", function(el, binding) {
+  // bind/updateに適用される
 });
 
 new Vue({
