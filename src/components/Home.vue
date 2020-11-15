028a4abd230e2ac0d31b52c8bc44c562a69f2b78
k-kudo-hub <tongariisaibou@gmail.com>
2020-11-15 09:21:33 +0900

    カスタムディレクティブをローカルで登録する

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -2,3 +2,22 @@
   <p v-border:dotted.round.shadow="{width: '5px', color: 'red'}">Home</p>
   <!-- （注）引数は一つしか取れない -->
 </template>
+
+<script>
+// ローカル登録。thisは使うことができない
+export default {
+  directives: {
+    border(el, binding) {
+      el.style.borderWidth = binding.value.width;
+      el.style.borderColor = binding.value.color;
+      el.style.borderStyle = binding.arg;
+      if (binding.modifiers.round) {
+        el.style.borderRadius = "0.5rem"
+      }
+      if (binding.modifiers.shadow) {
+        el.style.boxShadow = "0 2px 5px rgba(0,0,0,0.26)"
+      }
+    }
+  }
+};
+</script>
