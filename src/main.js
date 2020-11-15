3b470b87ce49a1e323654a660c967dd2e587d75e
k-kudo-hub <tongariisaibou@gmail.com>
2020-11-15 10:44:13 +0900

    グローバルミックスイン

--- src/main.js
+++ src/main.js
@@ -7,9 +7,14 @@ Vue.component('LikeNumber',LikeNumber);
 Vue.filter("upperCase", function(value) {
   return value.toUpperCase();
 });
+Vue.mixin({
+  created() {
+    console.log("global mixin");
+  }
+});
 
 new Vue({
-  render: h => h(App),
+  render: h => h(App)
 }).$mount('#app');
 
 
